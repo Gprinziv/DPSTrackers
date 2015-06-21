7273cdd73eee614d6c28ae2a381e3e074481daf1
gprinziv <Gprinziv@gmail.com>
2015-06-21 03:48:20 +0300

    Second Commit
    More skeleton work. Beginning work on Ashe class.

---
+++ Champions/Ashe.py
@@ -0,0 +1,18 @@
+"""
+Ashe.py - A class containing the basic statistics of an Ashe
+"""
+
+BASE_AD = 51.088
+BASE_AS = .658
+AD_LEVEL = 2.85
+AS_LEVEL = 3.33
+
+class Ashe:
+    
+    def __init__(self):
+
+
+def AsheCalculator():
+
+if __name__ == "__main__":
+    print "Run Tracker.py to generate a new Ashe. Feature to call directly will come later."
